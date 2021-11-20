f368d39d5e6c4bd3722933a893329f2d9a999c17
Casper Sparre <reptail@gmail.com>
2021-11-20 15:57:46 +0100

    Removed implicit operators.

--- src/CSDeskBand/DeskBandSize.cs
+++ src/CSDeskBand/DeskBandSize.cs
@@ -66,45 +66,5 @@
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
-
-#if DESKBAND_WPF
-        /// <summary>
-        /// Converts from <see cref="System.Windows.Size"/> to <see cref="DeskBandSize"/>.
-        /// </summary>
-        /// <param name="size">The <see cref="System.Windows.Size"/> to convert.</param>
-        public static implicit operator DeskBandSize(System.Windows.Size size)
-        {
-            return new DeskBandSize(Convert.ToInt32(size.Width), Convert.ToInt32(size.Height));
-        }
-
-        /// <summary>
-        /// Converts from <see cref="DeskBandSize"/> to <see cref="System.Windows.Size"/>.
-        /// </summary>
-        /// <param name="size">The <see cref="DeskBandSize"/> to convert.</param>
-        public static implicit operator System.Windows.Size(DeskBandSize size)
-        {
-            return new System.Windows.Size(size.Width, size.Height);
-        }
-#endif
-
-#if DESKBAND_WINFORMS
-        /// <summary>
-        /// Converts from <see cref="System.Drawing.Size"/> to <see cref="DeskBandSize"/>.
-        /// </summary>
-        /// <param name="size">The <see cref="System.Drawing.Size"/> to convert.</param>
-        public static implicit operator DeskBandSize(System.Drawing.Size size)
-        {
-            return new DeskBandSize(size.Width, size.Height);
-        }
-
-        /// <summary>
-        /// Converts from <see cref="DeskBandSize"/> to <see cref="System.Drawing.Size"/>.
-        /// </summary>
-        /// <param name="size">The <see cref="DeskBandSize"/> to convert.</param>
-        public static implicit operator System.Drawing.Size(DeskBandSize size)
-        {
-            return new System.Drawing.Size(size.Width, size.Height);
-        }
-#endif
     }
 }
